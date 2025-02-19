ca6a0472f444544675cbd1c7f5b84537ff5631a5
Majharulislam <majhuruislam535@gmail.com>
2025-02-19 10:38:50 +0600

    Sponsorship section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,21 +3,23 @@ import Faq_section from "./Faq_section";
 import Hero from "./Hero";
 import Newsletter_card from "./Newsletter_card";
 import Review_section from "./Review_section";
+import Sponsorship from "./Sponsorship";
 import Study_session_home from "./Study_session_home";
 import Tutor_home_section from "./Tutor_home_section";
 
- 
+
 
 const Home = () => {
     return (
         <div>
-             <Hero></Hero>
-             <Study_session_home></Study_session_home>
-             <Tutor_home_section></Tutor_home_section>
-             <Review_section></Review_section>
-             <Blog_Section></Blog_Section>
-             <Faq_section></Faq_section>
-             <Newsletter_card></Newsletter_card>
+            <Hero></Hero>
+            <Study_session_home></Study_session_home>
+            <Tutor_home_section></Tutor_home_section>
+            <Review_section></Review_section>
+            <Blog_Section></Blog_Section>
+            <Faq_section></Faq_section>
+            <Sponsorship></Sponsorship>
+            <Newsletter_card></Newsletter_card>
         </div>
     );
 };

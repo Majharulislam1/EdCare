92694eec667760f0cf7f880f8b8c599628bbc235
Majharulislam <majhurulislam535@gmail.com>
2025-05-01 01:12:02 +0600

    add Line chat

--- src/Components/Admin_Panel.jsx
+++ src/Components/Admin_Panel.jsx
@@ -3,6 +3,7 @@ import { FcTimeline } from "react-icons/fc";
 import { FcConferenceCall } from "react-icons/fc";
 import { FcDocument } from "react-icons/fc";
 import { FcComboChart } from "react-icons/fc";
+import CustomLineChart from './LineCharts';
 // import LineCharts from './LineChart';
 // import CustomBarChart from './BarCharts';
 // import DashboardWidgets from './TeamMembers';
@@ -23,7 +24,7 @@ const Admin_Panel = () => {
                     <div>
                         <p className='text-xl font-bold text-white'>Total Traffic</p>
                         <h4 className='text-4xl font-bold text-white'>34,500</h4>
-                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                        <p className='text-white'> <span className='text-black'>3.45% </span> science last Month</p>
                     </div>
                     <div>
                         <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
@@ -31,14 +32,11 @@ const Admin_Panel = () => {
                 </div>
 
 
-
-
-
-                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                <div className='bg-primary rounded-lg p-6 flex justify-between items-center'>
                     <div>
-                        <p>Total User</p>
-                        <h4>4,500</h4>
-                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                        <p className='text-xl font-bold text-white'>Total User</p>
+                        <h4 className='text-4xl font-bold text-white'>4,500</h4>
+                        <p className='text-white'> <span className='text-black'>3.45% </span> science last Month</p>
                     </div>
                     <div>
                         <FcConferenceCall className='bg-white text-6xl p-3 rounded-full' />
@@ -48,22 +46,22 @@ const Admin_Panel = () => {
 
 
 
-                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                <div className='bg-primary rounded-lg p-6 flex justify-between items-center'>
                     <div>
-                        <p>Total Post</p>
-                        <h4>2,570</h4>
-                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                        <p className='text-xl font-bold text-white'>Total Student</p>
+                        <h4 className='text-4xl font-bold text-white'>2,570</h4>
+                        <p className='text-white'> <span className='text-black'>3.45% </span> science last Month</p>
                     </div>
                     <div>
                         <FcDocument className='bg-white text-6xl p-3 rounded-full' />
                     </div>
                 </div>
 
-                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                <div className='bg-primary rounded-lg p-6 flex justify-between items-center'>
                     <div>
-                        <p>Performance</p>
-                        <h4>67%</h4>
-                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                        <p className='text-xl font-bold text-white'>Performance</p>
+                        <h4 className='text-4xl font-bold text-white'>67%</h4>
+                        <p className='text-white'> <span className='text-black'>3.45% </span> science last Month</p>
                     </div>
                     <div>
                         <FcComboChart className='bg-white text-6xl p-3 rounded-full' />
@@ -79,9 +77,9 @@ const Admin_Panel = () => {
 
 
             <div className="flex flex-col lg:flex-row my-7 justify-between items-center">
-                <div className="w-full lg:w-4/6 md:m-4">
+                <div className="w-full lg:w-4/6 md:m-4 lg:m-0">
                     
-                    {/* <LineCharts></LineCharts> */}
+                    <CustomLineChart></CustomLineChart>
                 </div>
                 <div className="w-full lg:w-2/6">
                     {/* <CustomBarChart></CustomBarChart> */}
